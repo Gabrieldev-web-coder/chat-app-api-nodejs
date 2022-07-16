354ab5c6d4717e12c1ba0ea9c2ea83f0871963e5
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-16 14:30:54 -0500

    removing unused packages and adding helmet library

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,18 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { body, validationResult } from "express-validator";
+import { checkUser } from "./database/database.queries.api.js";
 dotenv.config();
 const app = express();
 let port = process.env.PORT || 5000;
@@ -13,11 +24,38 @@ app.use((req, res, next) => {
     console.log(req.body);
     next();
 });
-app.post("/register-user", (req, res) => {
-});
-app.post("/login-user", (req, res) => {
-});
-app.post("/login-user-api", (req, res) => {
-});
-app.listen(port, () => console.log('running on http://localhost:' + port));
+app.post("/register-user", body("email")
+    .isEmail()
+    .isString()
+    .exists({ checkFalsy: true, checkNull: true })
+    .normalizeEmail(), body("username")
+    .isString()
+    .isLength({ min: 4 })
+    .exists({ checkFalsy: true, checkNull: true }), body("pwd")
+    .isString()
+    .isLength({ min: 8 })
+    .exists({ checkFalsy: true, checkNull: true }), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!validationResult(req).isEmpty()) {
+        res
+            .status(401)
+            .json({
+            message: "No authorized",
+            errors: validationResult(req).array(),
+        });
+    }
+    else {
+        res.status(200).json({ message: 'You visit me since: ' + req.headers["access-control-allow-credentials"] });
+    }
+}));
+app.post("/login-user", body("username").isString(), body("pwduser").isStrongPassword(), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!validationResult(req).isEmpty())
+        res.status(401).json({
+            message: "No authorized",
+            errors: validationResult(req).array(),
+        });
+    const body = req.body;
+    checkUser(body);
+}));
+app.post("/login-user-api", (req, res) => __awaiter(void 0, void 0, void 0, function* () { }));
+app.listen(port, () => console.log("running on http://localhost:" + port));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { body, validationResult } from "express-validator";
-import { checkUser } from "./database/database.queries.api";
-
+import { checkUser } from "./database/database.queries.api.js";
+import helmet from "helmet";
 dotenv.config();
 
 const app = express();
 let port = process.env.PORT || 5000;
 
 app.use(cors({ origin: "*", credentials: true }));
-app.use(cookieParser());
+app.use(helmet());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
@@ -25,7 +25,8 @@ app.post(
   body("email")
     .isEmail()
     .isString()
-    .exists({ checkFalsy: true, checkNull: true }),
+    .exists({ checkFalsy: true, checkNull: true })
+    .normalizeEmail(),
   body("username")
     .isString()
     .isLength({ min: 4 })
@@ -34,7 +35,18 @@ app.post(
     .isString()
     .isLength({ min: 8 })
     .exists({ checkFalsy: true, checkNull: true }),
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    if (!validationResult(req).isEmpty()) {
+      res.status(401).json({
+        message: "No authorized",
+        errors: validationResult(req).array(),
+      });
+    } else {
+      res.status(200).json({
+        message: "Request received.",
+      });
+    }
+  }
 );
 
 app.post(
