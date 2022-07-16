212cf36f7abf0d5d8a62d80300168936c0ece7a4
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-16 18:31:42 -0500

    fixing uuid integer

--- src/middlewares/set.default.ts
+++ src/middlewares/set.default.ts
@@ -7,11 +7,11 @@ const { lookup } = geoip;
 const setDefault = async (req: Request) => {
   const ip = getClientIp(req);
   const idAssign = Number(
-    String(uuidint(0).uuid()).split("").splice(0, 4).join("").toLowerCase()
+    String(uuidint(0).uuid()).split("").splice(12, 4).join("")
   );
   req.body.userid = idAssign;
   req.body.picurl = "https://svgsilh.com/svg/1699635.svg";
-  req.body.country = lookup(ip).country;
+  req.body.country = lookup(ip).country.toLowerCase();
   req.body.groups = [];
   req.body.friends = [];
   return req.body;
