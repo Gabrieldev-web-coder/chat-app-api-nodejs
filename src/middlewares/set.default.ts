d2fc797510efabd1e71d33e4d440459fa4337224
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-16 17:52:03 -0500

    removing response param from set dafault method to mutate body

--- src/middlewares/set.default.ts
+++ src/middlewares/set.default.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from "express";
+import { Request } from "express";
 import geoip from "geoip-lite";
 import { getClientIp } from "request-ip";
 import uuidint from "uuid-int";
 const { lookup } = geoip;
 
-const setDefault = async (req: Request, res: Response) => {
+const setDefault = async (req: Request) => {
   const ip = getClientIp(req);
   const idAssign = Number(
     String(uuidint(0).uuid()).split("").splice(0, 4).join("").toLowerCase()
