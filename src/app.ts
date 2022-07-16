da7d9e8f1098ca225d323858bb03d438eb827206
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-16 16:35:56 -0500

    removing packages for merging testing branches

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Response, Request } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 import { body, validationResult } from "express-validator";
 import { checkUser } from "./database/database.queries.api";
 
@@ -11,7 +10,6 @@ const app = express();
 let port = process.env.PORT || 5000;
 
 app.use(cors({ origin: "*", credentials: true }));
-app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
