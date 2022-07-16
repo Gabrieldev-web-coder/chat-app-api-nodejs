8e37f247009969bfa5fdc60118cb4a7e393ddc6b
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-16 11:07:44 -0500

    setting options

--- src/database/database.queries.api.ts
+++ src/database/database.queries.api.ts
@@ -1,98 +1,125 @@
-import { MongoClient, ServerApiVersion, MongoClientOptions } from 'mongodb'
-import { cred,userData,generateUser } from "../schemas/cred.user.js";
+import { MongoClient, ServerApiVersion, MongoClientOptions } from "mongodb";
+import { cred, userData, generateUser } from "../schemas/cred.user.js";
 import { Observable } from "rxjs";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const checkUserApi = (userCredentials: cred) => {
   return new Observable((suscriber) => {
-    const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } as MongoClientOptions);
-    client.connect(async err => {
-      if (err) suscriber.error(err.name + ' ' + err.message)
-      const collection = client.db(process.env.DB_API).collection(process.env.DB_COLLECTION_API);
-      await collection.findOne({
-        $and: [{ username: userCredentials.username, pwd: userCredentials.pwd }]
-      })
+    const client = new MongoClient(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    } as MongoClientOptions);
+    client.connect(async (err) => {
+      if (err) suscriber.error(err.name + " " + err.message);
+      const collection = client
+        .db(process.env.DB_API)
+        .collection(process.env.DB_COLLECTION_API);
+      await collection
+        .findOne({
+          $and: [
+            { username: userCredentials.username, pwd: userCredentials.pwd },
+          ],
+        })
         .catch((err) => suscriber.error(err))
         .then((value) => suscriber.next(value))
         .finally(() => {
           client.close().finally(() => {
             suscriber.complete();
-          })
-        })
+          });
+        });
     });
-
-  })
+  });
 };
 
 const checkUser = (userCredentials: cred) => {
   return new Observable((suscriber) => {
-    const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } as MongoClientOptions);
-    client.connect(async err => {
-      if (err) suscriber.error(err.name + ' ' + err.message)
-      const collection = client.db(process.env.DB_API).collection(process.env.DB_COLLECTION_API);
-      await collection.findOne({
-         username: userCredentials.username, pwd: userCredentials.pwd 
-      })
+    const client = new MongoClient(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    } as MongoClientOptions);
+    client.connect(async (err) => {
+      if (err) suscriber.error(err.name + " " + err.message);
+      const collection = client
+        .db(process.env.DB_API)
+        .collection(process.env.DB_COLLECTION_API);
+      await collection
+        .findOne({
+          username: userCredentials.username,
+          pwd: userCredentials.pwd,
+        })
         .catch((err) => suscriber.error(err))
         .then((value) => suscriber.next(value))
         .finally(() => {
           client.close().finally(() => {
             suscriber.complete();
-          })
-        })
+          });
+        });
     });
-
-  })
+  });
 };
 
-const saveDataUser = (userInfo:userData) => {
-  return new Observable((suscriber)=>{
-    const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } as MongoClientOptions);
-    client.connect(async err => {
-      if (err) suscriber.error(err.name + ' ' + err.message)
-      const collection = client.db(process.env.DB_API).collection(process.env.DB_COLLECTION_API);
-      let user
+const saveDataUser = (userInfo: userData) => {
+  return new Observable((suscriber) => {
+    const client = new MongoClient(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    } as MongoClientOptions);
+    client.connect(async (err) => {
+      if (err) suscriber.error(err.name + " " + err.message);
+      const collection = client
+        .db(process.env.DB_API)
+        .collection(process.env.DB_COLLECTION_API);
+      let user;
       await generateUser(userInfo)
-      .then((value) => user = value)
-      .catch((err)=> suscriber.error(err))
+        .then((value) => (user = value))
+        .catch((err) => suscriber.error(err));
 
-      await collection.insertOne({user})
+      await collection
+        .insertOne({ user })
         .catch((err) => suscriber.error(err))
         .then((value) => suscriber.next(value))
         .finally(() => {
           client.close().finally(() => {
             suscriber.complete();
-          })
-        })
+          });
+        });
     });
-  })
-}
+  });
+};
 
-const saveDataApiUser = (userInfo:userData) => {
-  return new Observable((suscriber)=>{
-    const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } as MongoClientOptions);
-    client.connect(async err => {
-      if (err) suscriber.error(err.name + ' ' + err.message)
-      const collection = client.db(process.env.DB_API).collection(process.env.DB_COLLECTION_API);
-      let user
+const saveDataApiUser = (userInfo: userData) => {
+  return new Observable((suscriber) => {
+    const client = new MongoClient(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    } as MongoClientOptions);
+    client.connect(async (err) => {
+      if (err) suscriber.error(err.name + " " + err.message);
+      const collection = client
+        .db(process.env.DB_API)
+        .collection(process.env.DB_COLLECTION_API);
+      let user;
       await generateUser(userInfo)
-      .then((value) => user = value)
-      .catch((err)=> suscriber.error(err))
+        .then((value) => (user = value))
+        .catch((err) => suscriber.error(err));
 
-      await collection.insertOne({user})
+      await collection
+        .insertOne({ user })
         .catch((err) => suscriber.error(err))
         .then((value) => suscriber.next(value))
         .finally(() => {
           client.close().finally(() => {
             suscriber.complete();
-          })
-        })
+          });
+        });
     });
-  })
-}
+  });
+};
 
-export {checkUserApi,checkUser,saveDataApiUser}
-
-
+export { checkUserApi, checkUser, saveDataApiUser };
--- src/database/database.queries.register.ts
+++ src/database/database.queries.register.ts
@@ -1,15 +1,18 @@
-import {MongoClient} from "mongodb";
-import * as bcrypt from "bcrypt"
+import { MongoClient } from "mongodb";
+import * as bcrypt from "bcrypt";
 
-const CheckUserInDatabase = (dbUrl:string,dbName:string,collectionName:string,username:string,userpassword:string)=>{
-    const MongoDB = new MongoClient(dbUrl)
+const CheckUserInDatabase = (
+  dbUrl: string,
+  dbName: string,
+  collectionName: string,
+  username: string,
+  userpassword: string
+) => {
+  const MongoDB = new MongoClient(dbUrl)
     .db(dbName)
     .collection(collectionName)
-    .findOne({username:username,password:userpassword},(err,result)=>{
-        if(err) console.log(err)
-        console.log(result) 
-    })
-}
-
-
-
+    .findOne({ username: username, password: userpassword }, (err, result) => {
+      if (err) console.log(err);
+      console.log(result);
+    });
+};
