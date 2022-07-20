b179b81e1b1bce3446e64b82acec634b1a77f199
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-19 21:03:43 -0500

    adding routes folder

--- dist/app.js
+++ dist/app.js
@@ -1,19 +1,9 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { body, validationResult } from "express-validator";
-import { checkUser } from "./database/database.queries.api.js";
 import helmet from "helmet";
-import { generateUser } from './middlewares/generate.user.js';
+import register from "./routes/register.user.js";
+import login from "./routes/login.users.js";
 dotenv.config();
 const app = express();
 let port = process.env.PORT || 5000;
@@ -21,41 +11,7 @@ app.use(cors({ origin: "*", credentials: true }));
 app.use(helmet());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
-app.use((req, res, next) => {
-    console.log(req.body);
-    next();
-});
-app.post("/register-user", body("email")
-    .isEmail()
-    .isString()
-    .exists({ checkFalsy: true, checkNull: true })
-    .normalizeEmail(), body("username")
-    .isString()
-    .isLength({ min: 4 })
-    .exists({ checkFalsy: true, checkNull: true }), body("pwd")
-    .isString()
-    .isLength({ min: 8 })
-    .exists({ checkFalsy: true, checkNull: true }), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    if (!validationResult(req).isEmpty()) {
-        res.status(401).json({
-            message: "No authorized",
-            errors: validationResult(req).array(),
-        });
-    }
-    else {
-        const body = yield generateUser(req);
-        res.status(200).json({ body });
-    }
-}));
-app.post("/login-user", body("username").isString(), body("pwduser").isStrongPassword(), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    if (!validationResult(req).isEmpty())
-        res.status(401).json({
-            message: "No authorized",
-            errors: validationResult(req).array(),
-        });
-    const body = req.body;
-    checkUser(body);
-}));
-app.post("/login-user-api", (req, res) => __awaiter(void 0, void 0, void 0, function* () { }));
-app.listen(port, () => console.log("running on http://localhost:" + port));
+app.use(register);
+app.use(login);
+app.listen(port, () => console.log("http://localhost:" + port));
 //# sourceMappingURL=app.js.map
--- dist/database/database.queries.register.js
+++ dist/database/database.queries.register.js
@@ -1,12 +1,49 @@
-import { MongoClient } from "mongodb";
-const CheckUserInDatabase = (dbUrl, dbName, collectionName, username, userpassword) => {
-    const MongoDB = new MongoClient(dbUrl)
-        .db(dbName)
-        .collection(collectionName)
-        .findOne({ username: username, password: userpassword }, (err, result) => {
-        if (err)
-            console.log(err);
-        console.log(result);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+import { Observable } from "rxjs";
+dotenv.config();
+const registerUser = (user) => {
+    return new Observable((suscriber) => {
+        const client = new MongoClient(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverApi: ServerApiVersion.v1,
+        });
+        client.connect((err) => __awaiter(void 0, void 0, void 0, function* () {
+            if (err)
+                suscriber.error(err.name + " " + err.message);
+            const collection = client
+                .db(process.env.DB_REGISTER)
+                .collection(process.env.DB_COLLECTION_REGISTERED);
+            const checkingUser = collection.find({ user });
+            console.log(checkingUser);
+            if (!checkingUser) {
+                yield collection
+                    .insertOne({ user })
+                    .catch((err) => {
+                    suscriber.error(err);
+                })
+                    .then((value) => {
+                    suscriber.next(value);
+                });
+                yield client.close().finally(() => {
+                    suscriber.complete();
+                });
+            }
+            else {
+                suscriber.error('This user already exist, consider login.');
+            }
+        }));
+    });
+};
+export { registerUser };
 //# sourceMappingURL=database.queries.register.js.map
