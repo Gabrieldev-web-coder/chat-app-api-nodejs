8b32ade90dcc5c7da9239bd0510bb86b222e833b
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-19 20:09:12 -0500

    register user method created

--- src/database/database.queries.register.ts
+++ src/database/database.queries.register.ts
@@ -2,10 +2,11 @@ import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
 import * as bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import { Observable } from "rxjs";
+import { registerNewUser } from "../schemas/cred.user.js";
 
 dotenv.config();
 
-const registerUser = (user:RegisterNewUser) => {
+const registerUser = (user: registerNewUser) => {
   return new Observable((suscriber) => {
     const client = new MongoClient(process.env.DB_URL, {
       useNewUrlParser: true,
@@ -18,9 +19,14 @@ const registerUser = (user:RegisterNewUser) => {
       const collection = client
         .db(process.env.DB_REGISTER)
         .collection(process.env.DB_COLLECTION_REGISTERED);
-      collection.insertOne({
-
-      });
+      await collection
+        .insertOne({ user })
+        .catch((err) => {
+          suscriber.error(err);
+        })
+        .then((value) => {
+          suscriber.next(value);
+        });
       await client.close().finally(() => {
         suscriber.complete();
       });
