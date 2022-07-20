cc20533c0a8c970025e16f825b54c95881d71afd
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-19 21:41:04 -0500

    basic jwt authentication methods added

--- src/jwt.auth/jwt.ts
+++ src/jwt.auth/jwt.ts
@@ -1,2 +1,32 @@
-import * as jwt from 'jsonwebtoken'
+import * as jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { Observable } from "rxjs";
 
+dotenv.config();
+
+const generateToken = (body: string): Observable<string> => {
+  return new Observable((suscriber) => {
+    jwt.sign(
+      body,
+      process.env.SECRET,
+      { algorithm: "RS256" },
+      (err, payload) => {
+        if (err) return suscriber.error("Error generating jwt: " + err.message);
+        suscriber.next(payload);
+      }
+    );
+    suscriber.complete();
+  });
+};
+
+const verifyJwt = (token: string): Observable<string | jwt.JwtPayload> => {
+  return new Observable((suscriber) => {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    if (!decoded)
+      suscriber.error(
+        "Given token was modified or was nor provided by server."
+      );
+    suscriber.next(decoded);
+    suscriber.complete();
+  });
+};
