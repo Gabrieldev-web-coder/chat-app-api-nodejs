66df68531f1c1f8659bee52bc05b23f954c288a7
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-19 20:50:27 -0500

    create user method

---
+++ dist/routes/register.user.js
@@ -0,0 +1,41 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { Router } from "express";
+import { validationResult, body } from "express-validator";
+import { registerUser } from "../database/database.queries.register.js";
+import { generateUser } from "../middlewares/generate.user.js";
+const register = Router().post("/register-user", body("email")
+    .isEmail()
+    .isString()
+    .exists({ checkNull: true, checkFalsy: true })
+    .normalizeEmail(), body("username")
+    .isString()
+    .isLength({ min: 4 })
+    .exists({ checkNull: true, checkFalsy: true }), body("pwd")
+    .isString()
+    .isLength({ min: 8 })
+    .exists({ checkFalsy: true, checkNull: true }), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!validationResult(req).isEmpty()) {
+        res.status(401).json({
+            message: "No authorized",
+            errors: validationResult(req).array(),
+        });
+    }
+    else {
+        const body = yield generateUser(req);
+        registerUser(body)
+            .subscribe({
+            next: (value) => res.status(200).json({ result: value }),
+            error: (err) => res.status(501).json({ errors: err })
+        });
+    }
+}));
+export default register;
+//# sourceMappingURL=register.user.js.map
--- src/routes/register.user.ts
+++ src/routes/register.user.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { validationResult, body } from "express-validator";
-import { Response,Request } from "express";
+import { Response, Request } from "express";
+import { registerUser } from "../database/database.queries.register.js";
+import { generateUser } from "../middlewares/generate.user.js";
 
 const register = Router().post(
   "/register-user",
@@ -10,7 +12,6 @@ const register = Router().post(
     .exists({ checkNull: true, checkFalsy: true })
     .normalizeEmail(),
   body("username")
-    .isEmail()
     .isString()
     .isLength({ min: 4 })
     .exists({ checkNull: true, checkFalsy: true }),
@@ -18,14 +19,21 @@ const register = Router().post(
     .isString()
     .isLength({ min: 8 })
     .exists({ checkFalsy: true, checkNull: true }),
-    async (req:Request,res:Response)=>{
-      if (!validationResult(req).isEmpty()){
-        res.status(401).json({
-          message: "No authorized",
-          errors: validationResult(req).array(),
-        });
-      }
+  async (req: Request, res: Response) => {
+    if (!validationResult(req).isEmpty()) {
+      res.status(401).json({
+        message: "No authorized",
+        errors: validationResult(req).array(),
+      });
+    } else {
+      const body = await generateUser(req)
+      registerUser(body)
+      .subscribe({
+        next:(value) => res.status(200).json({result:value}),
+        error:(err) => res.status(501).json({errors:err})
+      })
     }
+  }
 );
 
-export default register 
+export default register;
