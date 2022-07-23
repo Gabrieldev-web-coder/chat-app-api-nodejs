b994e6ffd10b391fdd656510f25cb17f531c9ac6
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-22 20:47:13 -0500

    fields added to setDefaults method

--- src/middlewares/set.default.ts
+++ src/middlewares/set.default.ts
@@ -4,10 +4,11 @@ import { getClientIp } from "request-ip";
 import uuidint from "uuid-int";
 import { registerNewUser } from '../schemas/cred.user.js';
 import moment from 'moment';
+import { reqRegisterBody } from "../schemas/cred.user.js";
 
 const { lookup } = geoip;
 
-const setDefault = async (req: Request):Promise<registerNewUser> => {
+const setDefault = async (req: Request<reqRegisterBody>):Promise<registerNewUser> => {
   const ip = getClientIp(req);
   const idAssign = Number(
     String(uuidint(0).uuid()).split("").splice(12, 4).join("")
@@ -16,7 +17,7 @@ const setDefault = async (req: Request):Promise<registerNewUser> => {
   req.body.picurl = "https://svgsilh.com/svg/1699635.svg";
   req.body.country = lookup(ip).country.toLowerCase();
   req.body.accountCreatedAt = moment().format('LLLL')
-  req.body.description = `Hi! i'm someone, nice to meet you! :D`
+  req.body.description = `Hi! i'm ${req.body.username}, nice to meet you! :D`
   req.body.groups = [];
   req.body.friends = [];
   return req.body;
