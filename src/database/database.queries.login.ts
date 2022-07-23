60438b476d67ced0f15c7d9707e3d2d2b7fd1db0
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-22 21:19:03 -0500

    building login function

---
+++ src/database/database.queries.login.ts
@@ -0,0 +1,26 @@
+import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
+
+import { Observable } from "rxjs";
+import { Request } from "express";
+import dotenv from "dotenv";
+import { cred } from "../schemas/cred.user.js";
+
+dotenv.config();
+
+const checkUser = (userCred: cred): Observable<any> => {
+  return new Observable((suscriber) => {
+    const client = new MongoClient(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    } as MongoClientOptions);
+
+    client.connect(async (err) => {
+      if (err) suscriber.error(err.message + " " + err.name);
+      const collection = client
+        .db(process.env.DB_REGISTER)
+        .collection(process.env.DB_COLLECTION_REGISTERED);
+    });
+    const { username, pwd } = userCred;
+  });
+};
