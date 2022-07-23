30ac91a6b675eb2a79e9d92dbd3af09fbf18916f
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-22 21:00:14 -0500

    setting setDefault into promise function

--- src/middlewares/set.default.ts
+++ src/middlewares/set.default.ts
@@ -7,19 +7,21 @@ import moment from "moment";
 const { lookup } = geoip;
 
 const setDefault = async (req: Request): Promise<registerNewUser> => {
-  const ip = getClientIp(req);
-  const idAssign = Number(
-    String(uuidint(0).uuid()).split("").splice(12, 4).join("")
-  );
-  req.body.userid = idAssign;
-  req.body.picurl = "https://svgsilh.com/svg/1699635.svg";
-  req.body.country = lookup(ip).country.toLowerCase();
-  req.body.accountCreatedAt = moment().format("LLLL");
-  req.body.description = `Hi! i'm ${req.body.username}, nice to meet you! :D`;
-  req.body.lastMoficationAt = 'No changes yet.'
-  req.body.groups = [];
-  req.body.friends = [];
-  return req.body;
+  return new Promise((resolve) => {
+    const ip = getClientIp(req);
+    const idAssign = Number(
+      String(uuidint(0).uuid()).split("").splice(12, 4).join("")
+    );
+    req.body.userid = idAssign;
+    req.body.picurl = "https://svgsilh.com/svg/1699635.svg";
+    req.body.country = lookup(ip).country.toLowerCase();
+    req.body.accountCreatedAt = moment().format("LLLL");
+    req.body.description = `Hi! i'm ${req.body.username}, nice to meet you! :D`;
+    req.body.lastMoficationAt = "No changes yet.";
+    req.body.groups = [];
+    req.body.friends = [];
+    resolve(req.body);
+  });
 };
 
 export { setDefault };
