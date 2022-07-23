3b5f18f614d080343bd2f96c674d6d37bf2aac1d
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-22 20:51:08 -0500

    removing type of request

--- src/middlewares/set.default.ts
+++ src/middlewares/set.default.ts
@@ -2,13 +2,11 @@ import { Request, Response } from "express";
 import geoip from "geoip-lite";
 import { getClientIp } from "request-ip";
 import uuidint from "uuid-int";
-import { registerNewUser } from '../schemas/cred.user.js';
-import moment from 'moment';
-import { reqRegisterBody } from "../schemas/cred.user.js";
-
+import { registerNewUser } from "../schemas/cred.user.js";
+import moment from "moment";
 const { lookup } = geoip;
 
-const setDefault = async (req: Request<reqRegisterBody>):Promise<registerNewUser> => {
+const setDefault = async (req: Request): Promise<registerNewUser> => {
   const ip = getClientIp(req);
   const idAssign = Number(
     String(uuidint(0).uuid()).split("").splice(12, 4).join("")
@@ -16,8 +14,8 @@ const setDefault = async (req: Request<reqRegisterBody>):Promise<registerNewUser
   req.body.userid = idAssign;
   req.body.picurl = "https://svgsilh.com/svg/1699635.svg";
   req.body.country = lookup(ip).country.toLowerCase();
-  req.body.accountCreatedAt = moment().format('LLLL')
-  req.body.description = `Hi! i'm ${req.body.username}, nice to meet you! :D`
+  req.body.accountCreatedAt = moment().format("LLLL");
+  req.body.description = `Hi! i'm ${req.body.username}, nice to meet you! :D`;
   req.body.groups = [];
   req.body.friends = [];
   return req.body;
