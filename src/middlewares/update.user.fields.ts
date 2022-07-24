657db6effb3187ad78ddb8b9b65d72ff70249919
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-24 12:18:49 -0500

    template for function

---
+++ src/middlewares/update.user.fields.ts
@@ -0,0 +1,10 @@
+import { Observable } from "rxjs";
+import { Request } from "express";
+
+const updateUser = async (req:Request) => {
+    return new Observable((suscriber)=>{
+        // Function for database actions ...
+    })
+};
+
+export default updateUser
