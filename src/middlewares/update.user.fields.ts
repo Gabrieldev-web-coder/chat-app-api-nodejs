d088d672d61cad41b3e2ec3c970ad1d330d7df90
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-25 13:33:15 -0500

    min length reset

--- src/middlewares/update.user.fields.ts
+++ src/middlewares/update.user.fields.ts
@@ -4,6 +4,7 @@ import updateUser from "../database/database.queries.update.js";
 const updateUserFields = (req: Request) => {
   return new Promise(async (resolve, reject) => {
     const fieldsSelected = Object.entries(req.body);
+    console.log(fieldsSelected)
     await updateUser(fieldsSelected).then((ObserverResult) => {
       ObserverResult.subscribe({
         next: (modify) => resolve(modify),
