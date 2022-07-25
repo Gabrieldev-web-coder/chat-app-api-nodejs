61716c6d7662b6d3ab802f2ebe4590bfd391ed5b
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-25 18:28:30 -0500

    selection fields method added

--- src/middlewares/fields.selection.ts
+++ src/middlewares/fields.selection.ts
@@ -1,10 +1,13 @@
 import { Request } from "express";
+import { optionalFields } from "../schemas/cred.user.js";
 
-const selectFields = (keys:string[],req:Request) => {
-    const fields = {}
-    for (let i = 0; i < keys.length; i++) fields[keys[i]] = req.body[keys[i]];
-    return fields
-}
+const selectFields = (keys: string[], req: Request): optionalFields => {
+  const fields: optionalFields = {};
+  type objectKey = keyof typeof fields;
 
-export default selectFields
+  for (let i = 0; i < keys.length; i++) fields[keys[i] as objectKey] = req.body[keys[i]];
+  
+  return fields;
+};
 
+export default selectFields;
