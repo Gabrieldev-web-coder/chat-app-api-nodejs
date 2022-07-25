6c2e2fc43c01889b743c29860890d6d3beadbd3c
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-25 10:15:06 -0500

    adding promise to updateFields

--- src/middlewares/update.user.fields.ts
+++ src/middlewares/update.user.fields.ts
@@ -1,10 +1,17 @@
 import { Observable } from "rxjs";
 import { Request } from "express";
+import updateUser from "../database/database.queries.update.js";
 
-const updateUser = async (req:Request) => {
-    return new Observable((suscriber)=>{
-        // Function for database actions ...
-    })
+const updateUserFields = (req: Request) => {
+  return new Promise(async (resolve, reject) => {
+    const fieldsSelected = Object.entries(req.body);
+    await updateUser(fieldsSelected).then((ObserverResult) => {
+      ObserverResult.subscribe({
+        next: (modify) => resolve(modify),
+        error: (err) => reject(err),
+      });
+    });
+  });
 };
 
-export default updateUser
+export default updateUserFields;
