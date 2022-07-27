3fa888182df2d502cdb2dbb7197cf794b80eb294
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-26 23:09:08 -0500

    adding fucntion to get id user from request

--- src/database/database.queries.update.ts
+++ src/database/database.queries.update.ts
@@ -2,21 +2,17 @@ import dotenv from "dotenv";
 import { Request } from "express";
 import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
 import { Observable } from "rxjs";
-import { decode, JwtPayload } from "jsonwebtoken";
-import { optionalFields } from "../schemas/cred.user.js";
 
 dotenv.config();
 
 const updateUser = async (keys: string[], req: Request) => {
-  const userBody = decode(req.body.token, { complete: true });
-  const payload = userBody.payload as JwtPayload;
-  const { userid } = JSON.parse(payload.body);
+  const userid = await import("../middlewares/get.user.id.js").then((idFound) =>
+    idFound.default(req)
+  );
 
   const selectFields = await import("../middlewares/fields.selection.js").then(
     (select) => select.default(keys, req)
   );
-
-  console.log(Object.keys(selectFields))
 
   return new Observable((suscriber) => {
     const client = new MongoClient(process.env.DB_URL, {
@@ -31,10 +27,10 @@ const updateUser = async (keys: string[], req: Request) => {
         .db(process.env.DB_REGISTER)
         .collection(process.env.DB_COLLECTION_REGISTERED);
       await collection
-        .findOneAndUpdate(
+        .updateOne(
           { "user.userid": userid },
           {
-            $set:{selectFields},
+            $set: selectFields,
           }
         )
         .then((modify) => suscriber.next(modify))
--- src/middlewares/fields.selection.ts
+++ src/middlewares/fields.selection.ts
@@ -2,12 +2,19 @@ import { Request } from "express";
 import { optionalFields } from "../schemas/cred.user.js";
 
 const selectFields = (keys: string[], req: Request): optionalFields => {
+  delete req.body.token;
   const fields: optionalFields = {};
   type objectKey = keyof typeof fields;
+  for (let i = 0; i < keys.length; i++)
+    fields[keys[i] as objectKey] = req.body[keys[i]];
+  const entries = Object.keys(fields);
+  const prefixAdded = entries.map((field) => "user." + field);
+  const entriesValues = Object.values(fields);
+  const query: any = {};
+  for (let i = 0; i < entries.length; i++)
+    query[prefixAdded[i]] = entriesValues[i];
 
-  for (let i = 0; i < keys.length; i++)  fields[keys[i] as objectKey] = req.body[keys[i]];
-  delete fields?.["user.token"]
-  return fields;
+  return query;
 };
 
 export default selectFields;
