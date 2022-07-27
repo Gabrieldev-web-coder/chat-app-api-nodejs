668dd184a5faab1e7a9caeb8c1f3ba0b18ebd970
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-26 20:29:11 -0500

    adding prefiex user. to query selection fields

--- src/middlewares/fields.selection.ts
+++ src/middlewares/fields.selection.ts
@@ -5,7 +5,7 @@ const selectFields = (keys: string[], req: Request): optionalFields => {
   const fields: optionalFields = {};
   type objectKey = keyof typeof fields;
 
-  for (let i = 0; i < keys.length; i++) fields[keys[i] as objectKey] = req.body[keys[i]];
+  for (let i = 0; i < keys.length; i++)  fields[`user.${keys[i] as objectKey}`] = req.body[keys[i]];
   delete fields?.token
   return fields;
 };
