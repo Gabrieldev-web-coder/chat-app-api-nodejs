29d0599b988a9182244d18516a8c6ebf49c603ac
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-26 20:23:22 -0500

    waiting for generate query mongodb

--- src/database/database.queries.update.ts
+++ src/database/database.queries.update.ts
@@ -2,13 +2,23 @@ import dotenv from "dotenv";
 import { Request } from "express";
 import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
 import { Observable } from "rxjs";
+import { decode, JwtPayload } from "jsonwebtoken";
+import { optionalFields } from "../schemas/cred.user.js";
 
 dotenv.config();
 
 const updateUser = async (keys: string[], req: Request) => {
+  const userBody = decode(req.body.token, { complete: true });
+  const payload = userBody.payload as JwtPayload;
+  const { userid } = JSON.parse(payload.body);
+  console.log(userid);
+
   const selectFields = await import("../middlewares/fields.selection.js").then(
     (select) => select.default(keys, req)
   );
+
+  console.log(selectFields);
+
   return new Observable((suscriber) => {
     const client = new MongoClient(process.env.DB_URL, {
       useNewUrlParser: true,
@@ -22,7 +32,12 @@ const updateUser = async (keys: string[], req: Request) => {
         .db(process.env.DB_REGISTER)
         .collection(process.env.DB_COLLECTION_REGISTERED);
       await collection
-        .findOneAndUpdate({}, {}) //Quering and update doc of user
+        .findOneAndUpdate(
+          { "user.userid": userid },
+          {
+            $set: {},
+          }
+        )
         .then((modify) => suscriber.next(modify))
         .catch((err) => suscriber.error(err));
       await client.close().finally(() => {
