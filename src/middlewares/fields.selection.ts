950af3a352eeaf67d2c4daff9d2218b57a3d2ff3
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-07-26 21:17:23 -0500

    modifing interface to add prefix user. to query

--- src/middlewares/fields.selection.ts
+++ src/middlewares/fields.selection.ts
@@ -5,8 +5,8 @@ const selectFields = (keys: string[], req: Request): optionalFields => {
   const fields: optionalFields = {};
   type objectKey = keyof typeof fields;
 
-  for (let i = 0; i < keys.length; i++)  fields[`user.${keys[i] as objectKey}`] = req.body[keys[i]];
-  delete fields?.token
+  for (let i = 0; i < keys.length; i++)  fields[keys[i] as objectKey] = req.body[keys[i]];
+  delete fields?.["user.token"]
   return fields;
 };
 
