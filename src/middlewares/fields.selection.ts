08443095e9e5ae4ccaae223b1be45a826a1c2594
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-07 11:27:25 -0500

    setting profile modification values

--- src/middlewares/fields.selection.ts
+++ src/middlewares/fields.selection.ts
@@ -1,13 +1,16 @@
 import { Request } from "express";
 import { optionalFields } from "../schemas/cred.user.js";
+import moment from "moment";
 
 const selectFields = (keys: string[], req: Request): optionalFields => {
   delete req.body.token;
   const fields: optionalFields = {};
+  fields["user.profileModifiedAt"] = `Last modification at: ${moment().format(
+    "LLLL"
+  )}`;
   type objectKey = keyof typeof fields;
   for (let i = 0; i < keys.length; i++) {
-    if (req.body[keys[i]])
-      fields[keys[i] as objectKey] = req.body[keys[i]];
+    if (req.body[keys[i]]) fields[keys[i] as objectKey] = req.body[keys[i]];
   }
   const entries = Object.keys(fields);
   const prefixAdded = entries.map((field) => "user." + field);
@@ -15,7 +18,6 @@ const selectFields = (keys: string[], req: Request): optionalFields => {
   const query: any = {};
   for (let i = 0; i < entries.length; i++)
     query[prefixAdded[i]] = entriesValues[i];
-
   return query;
 };
 
