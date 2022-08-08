e673198320db77dd478d33edb674f5ba00a417bd
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-08 11:01:44 -0500

    building request and response friend request queries

---
+++ src/database/database.queries.response.user.ts
@@ -0,0 +1,44 @@
+// import { Observable } from "rxjs";
+// import { FriendRequest } from "../schemas/cred.user.js";
+// import { Request } from "express";
+// import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const sendResponse = (req: Request): Observable<boolean> => {
+//   const userRequest = req.body as FriendRequest;
+//   return new Observable((suscriber) => {
+//     const client = new MongoClient(process.env.DB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       serverApi: ServerApiVersion.v1,
+//     } as MongoClientOptions);
+//     client.connect(async (err) => {
+//       if (err) suscriber.error(err.name + " " + err.message);
+//       const collection = client
+//         .db(process.env.DB_REGISTER)
+//         .collection(process.env.DB_COLLECTION_REGISTERED);
+
+//       const userid = userRequest.to;
+//       await collection
+//         .updateOne(
+//           { "user.userid": userid },
+//           { $push: { "user.friendRequest": userRequest } }
+//         )
+//         .then((updateResponse) => {
+//           if (updateResponse.acknowledged.valueOf()) suscriber.next(true);
+//         })
+//         .catch((err) => {
+//           suscriber.error(err.message + " " + err.name);
+//         })
+//         .finally(() => {
+//           client.close().finally(() => {
+//             suscriber.complete();
+//           });
+//         });
+//     });
+//   });
+// };
+
+// export default sendResponse;
---
+++ src/database/database.queries.set.request.ts
@@ -0,0 +1,42 @@
+import { Observable } from "rxjs";
+import { FriendRequest } from "../schemas/cred.user.js";
+import { Request } from "express";
+import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const setPendingRequest = (req: Request): Promise<boolean> => {
+  const userRequest = req.body as FriendRequest;
+  return new Promise((resolve, reject) => {
+    const client = new MongoClient(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    } as MongoClientOptions);
+    client.connect(async (err) => {
+      if (err) reject(err.name + " " + err.message);
+      const collection = client
+        .db(process.env.DB_REGISTER)
+        .collection(process.env.DB_COLLECTION_REGISTERED);
+
+      const { userid } = userRequest.from;
+      await collection
+        .updateOne(
+          { "user.userid": userid },
+          { $push: { "user.pendingRequest": userRequest } }
+        )
+        .then((updateResponse) => {
+          if (updateResponse.acknowledged.valueOf()) resolve(true);
+        })
+        .catch((err) => {
+          reject(err.message + " " + err.name);
+        })
+        .finally(() => {
+          client.close();
+        });
+    });
+  });
+};
+
+export default setPendingRequest;
