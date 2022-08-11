59eb3ed69af79ff340aef45588900942320f0e9c
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-11 17:01:12 -0500

    adding basic validation response friend request

--- src/routes/response.request.users.ts
+++ src/routes/response.request.users.ts
@@ -1,8 +1,24 @@
 import { Router } from "express";
+import { validationResult, body, header } from "express-validator";
+import validateJwt from "../middlewares/check.jwt";
 
 const userFriendResponse = Router().post(
   "/chat-api/v1.0/friend-response",
-  (req, res) => {}
+  header("Authorization"),
+  body("token")
+    .isJWT()
+    .isString()
+    .exists({ checkNull: true, checkFalsy: true })
+    .custom(validateJwt),
+  body("id").isNumeric().exists({ checkNull: true, checkFalsy: true }),
+  body("accepted").isBoolean().exists({ checkNull: false }),
+  (req, res) => {
+    if (!validationResult(req).isEmpty()) {
+      res.status(401).json({ errors: validationResult(req).array() });
+    } else {
+      //...
+    }
+  }
 );
 
 export default userFriendResponse;
