beca246c82f2f715f7a5c6abbaa5535a3c854f9f
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-11 09:11:14 -0500

    adding basic response to friend request route

--- src/routes/send.request.users.ts
+++ src/routes/send.request.users.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import sendRequest from "../database/database.queries.request.user.js";
 
 const userFriendRequest = Router().post(
   "/chat-api/v1.0/friend-request",
-  (req, res) => {}
+  (req, res) => {
+    sendRequest(req)
+      .subscribe({
+        next: (success) => res.status(200).json({ message: success ? "Friend request send successfully!" : "Cannot send friend request. Try later." }),
+        error: (err) => res.status(501).json({ error: err })
+      })
+  }
 );
 
 export default userFriendRequest;
