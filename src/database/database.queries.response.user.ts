b3185d5685151f91f6b74103d04c3fcc055372cf
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-12 20:33:51 -0500

    modifing friend request response

--- src/database/database.queries.response.user.ts
+++ src/database/database.queries.response.user.ts
@@ -1,37 +1,42 @@
-// import { Observable } from "rxjs";
-// import { FriendRequest } from "../schemas/cred.user.js";
-// import { Request } from "express";
-//import mongoClient from "../services/client.service.js";
+import { Observable } from "rxjs";
+import { Request } from "express";
+import removeUserRequest from "./database.queries.remove.request.user.js";
+import mongoClient from "../services/client.service.js";
 
-// const sendResponse = (req: Request): Observable<boolean> => {
-//   const userRequest = req.body as FriendRequest;
-//   return new Observable((suscriber) => {
-//     const client = mongoClient
-//     client.connect(async (err) => {
-//       if (err) suscriber.error(err.name + " " + err.message);
-//       const collection = client
-//         .db(process.env.DB_REGISTER)
-//         .collection(process.env.DB_COLLECTION_REGISTERED);
+const sendResponse = (req: Request): Observable<boolean> => {
+  const { id, emitterId } = req.body;
+  return new Observable((suscriber) => {
+    const client = mongoClient;
+    client.connect(async (err) => {
+      if (err) suscriber.error(err.name + " " + err.message);
+      const collection = client
+        .db(process.env.DB_REGISTER)
+        .collection(process.env.DB_COLLECTION_REGISTERED);
 
-//       const userid = userRequest.to;
-//       await collection
-//         .updateOne(
-//           { "user.userid": userid },
-//           { $push: { "user.friendRequest": userRequest } }
-//         )
-//         .then((updateResponse) => {
-//           if (updateResponse.acknowledged.valueOf()) suscriber.next(true);
-//         })
-//         .catch((err) => {
-//           suscriber.error(err.message + " " + err.name);
-//         })
-//         .finally(() => {
-//           client.close().finally(() => {
-//             suscriber.complete();
-//           });
-//         });
-//     });
-//   });
-// };
+      const removedFromPending = await removeUserRequest(req).then(
+        (updated) => updated
+      );
 
-// export default sendResponse;
+      if (removedFromPending) {
+        await collection
+          .updateOne(
+            { "user.userid": emitterId },
+            { $pull: { "user.friendRequest.?.from": id } }
+          )
+          .then((updateResponse) => {
+            if (updateResponse.acknowledged.valueOf()) suscriber.next(true);
+          })
+          .catch((err) => {
+            suscriber.error(err.message + " " + err.name);
+          })
+          .finally(() => {
+            client.close().finally(() => {
+              suscriber.complete();
+            });
+          });
+      }
+    });
+  });
+};
+
+export default sendResponse;
