f952b10e43f6d4b2073f905af7c00f11c053d3f1
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-17 12:14:04 -0500

    adding type condition for quering

--- src/database/db.qr.remove.pending.ts
+++ src/database/db.qr.remove.pending.ts
@@ -5,15 +5,65 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const removePendingUser = (req: Request, type: string): Observable<Boolean> => {
+const removePendingUser = (
+  req: Request,
+  type: string
+): Observable<Boolean | string> => {
   return new Observable((suscriber) => {
     const client = mongoClient;
+    const { id, emitterId, accepted } = req.body;
     client.connect(async (err) => {
       if (err) suscriber.error(err.name + " " + err.message);
       const collection = client
         .db(process.env.DB_REGISTER)
         .collection(process.env.DB_COLLECTION_REGISTERED as string);
-      //Some stuff...
+      if (type === "Pending request") {
+        await collection
+          .updateOne(
+            {
+              $and: [
+                { "user.userid": emitterId },
+                { "user.pendingRequest": { $elemMatch: { to: id } } },
+              ],
+            },
+            {
+              $unset: { "user.pendingRequest": { $elemMatch: { to: id } } },
+            }
+          )
+          .then((update) => {
+            update.modifiedCount > 0
+              ? suscriber.next(true)
+              : suscriber.next(false);
+          })
+          .catch((err) => {
+            suscriber.error(err.message);
+          });
+      } else if (type === "Friend request") {
+        await collection
+          .updateOne(
+            {
+              $and: [
+                { "user.userid": emitterId },
+                { "user.friendRequest": { $elemMatch: { from: id } } },
+              ],
+            },
+            {
+              $unset: {
+                "user.friendRequest": { $elemMatch: { from: id } },
+              },
+            }
+          )
+          .then((update) => {
+            update.modifiedCount > 0
+              ? suscriber.next(true)
+              : suscriber.next(false);
+          })
+          .catch((err) => {
+            suscriber.error(err.message);
+          });
+      } else {
+        suscriber.error(`${type} type dont exist in this protocol.`);
+      }
     });
   });
 };
