5cd01433335f02fe237a8803a1bb38b5df1bc8b0
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-20 02:06:20 -0500

    solving login issue

--- src/database/db.qr.login.ts
+++ src/database/db.qr.login.ts
@@ -26,30 +26,32 @@ const checkUser = (req: Request): Observable<loginResponse> => {
           .filter({ "user.username": value })
           .toArray(async (err, users) => {
             if (err) suscriber.error(err);
-            if (!users[0])
+            if (users.length === 0) {
               suscriber.error("This user don't exist, consider register");
-            const hashedPwd: string = users[0].user.pwd;
-            await verifyPwd(plainpwd, hashedPwd).then(async (value) => {
-              if (value) {
-                delete users[0].user.pwd;
-                const body = JSON.stringify(users[0].user);
-                let tokenResponse: string = "";
-                generateToken(body).subscribe({
-                  next: (token) => (tokenResponse = token),
-                });
-                const response: loginResponse = users[0].user;
-                response.token = tokenResponse;
-                suscriber.next(response);
-                await client.close().finally(() => {
-                  suscriber.complete();
-                });
-              } else {
-                await client.close().finally(() => {
-                  suscriber.error("Incorrect password.");
-                  suscriber.complete();
-                });
-              }
-            });
+            } else {
+              const hashedPwd: string = users[0].user.pwd;
+              await verifyPwd(plainpwd, hashedPwd).then(async (value) => {
+                if (value) {
+                  delete users[0].user.pwd;
+                  const body = JSON.stringify(users[0].user);
+                  let tokenResponse: string = "";
+                  generateToken(body).subscribe({
+                    next: (token) => (tokenResponse = token),
+                  });
+                  const response: loginResponse = users[0].user;
+                  response.token = tokenResponse;
+                  suscriber.next(response);
+                  await client.close().finally(() => {
+                    suscriber.complete();
+                  });
+                } else {
+                  await client.close().finally(() => {
+                    suscriber.error("Incorrect password.");
+                    suscriber.complete();
+                  });
+                }
+              });
+            }
           });
       } else {
         collection
@@ -57,28 +59,30 @@ const checkUser = (req: Request): Observable<loginResponse> => {
           .filter({ "user.email": value })
           .toArray(async (err, users) => {
             if (err) suscriber.error(err);
-            const hashedPwd: string = users[0].user.pwd;
-            await verifyPwd(plainpwd, hashedPwd).then(async (value) => {
-              if (value) {
-                delete users[0].user.pwd;
-                const body = JSON.stringify(users[0].user);
-                let tokenResponse: string = "";
-                generateToken(body).subscribe({
-                  next: (token) => (tokenResponse = token),
-                });
-                const response: loginResponse = users[0].user;
-                response.token = tokenResponse;
-                suscriber.next(response);
-                await client.close().finally(() => {
-                  suscriber.complete();
-                });
-              } else {
-                await client.close().finally(() => {
-                  suscriber.error("Incorrect password.");
-                  suscriber.complete();
-                });
-              }
-            });
+            if (users.length > 0) {
+              const hashedPwd: string = users[0].user.pwd;
+              await verifyPwd(plainpwd, hashedPwd).then(async (value) => {
+                if (value) {
+                  delete users[0].user.pwd;
+                  const body = JSON.stringify(users[0].user);
+                  let tokenResponse: string = "";
+                  generateToken(body).subscribe({
+                    next: (token) => (tokenResponse = token),
+                  });
+                  const response: loginResponse = users[0].user;
+                  response.token = tokenResponse;
+                  suscriber.next(response);
+                  await client.close().finally(() => {
+                    suscriber.complete();
+                  });
+                } else {
+                  await client.close().finally(() => {
+                    suscriber.error("Incorrect password.");
+                    suscriber.complete();
+                  });
+                }
+              });
+            }
           });
       }
     });
