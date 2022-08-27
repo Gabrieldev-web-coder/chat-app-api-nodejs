5e9dda81896e010d8e873e92a2d8a237b4c5ed0a
Gabrieldev-web-coder <gabrielgaymer04@gmail.com>
2022-08-27 18:04:13 -0500

    routes with functionality implementation

--- src/routes/add.friends.users.ts
+++ src/routes/add.friends.users.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import dotenv from "dotenv";
 import validateJwt from "../middlewares/check.jwt";
 import { validationResult, header, body } from "express-validator";
+import addFriendList from "../database/db.qr.add.friend";
 
 dotenv.config();
 
@@ -15,12 +16,22 @@ const addFriend = Router().post(
     .custom(validateJwt),
   body("id").isNumeric().exists({ checkNull: true, checkFalsy: true }),
   body("emitterId").isNumeric().exists({ checkNull: true, checkFalsy: true }),
-  body("type").isString().exists({ checkNull: true, checkFalsy: true }),
   (req, res) => {
     if (!validationResult(req).isEmpty()) {
       res.status(401).json({ errors: validationResult(req).array() });
     } else {
-      //Some stuff...
+      const { id } = req.body;
+      addFriendList(req).subscribe({
+        next: (updated) =>
+          updated
+            ? res.status(200).json({
+                message: `You added user ${id} to your friend list!`,
+              })
+            : res.status(501).json({
+                message: `Maybe you already have this user in your friend list`,
+              }),
+        error: (err) => res.status(501).json({ error: err }),
+      });
     }
   }
 );
--- src/routes/remove.friends.users.ts
+++ src/routes/remove.friends.users.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import dotenv from "dotenv";
 import validateJwt from "../middlewares/check.jwt";
 import { validationResult, header, body } from "express-validator";
+import removeFriendList from "../database/db.qr.remove.friend.js";
 
 dotenv.config();
 
@@ -15,12 +16,22 @@ const removeFriend = Router().post(
     .custom(validateJwt),
   body("id").isNumeric().exists({ checkNull: true, checkFalsy: true }),
   body("emitterId").isNumeric().exists({ checkNull: true, checkFalsy: true }),
-  body("type").isString().exists({ checkNull: true, checkFalsy: true }),
   (req, res) => {
     if (!validationResult(req).isEmpty()) {
       res.status(401).json({ errors: validationResult(req).array() });
     } else {
-      //Some stuff...
+      const { id } = req.body;
+      removeFriendList(req).subscribe({
+        next: (updated) =>
+          updated
+            ? res.status(200).json({
+                message: `You have removed user ${id} from your friend list!`,
+              })
+            : res.status(501).json({
+                message: `Maybe you dont have ${id} user in your friend list`,
+              }),
+        error: (err) => res.status(501).json({ error: err }),
+      });
     }
   }
 );
